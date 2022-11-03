0d057bebfc01e1efc7ac71819c5df30f0339d5b4
postive12 <48992839+postive12@users.noreply.github.com>
2022-11-03 17:30:39 +0900

    test

--- src/main/java/org/network/Main.java
+++ src/main/java/org/network/Main.java
@@ -15,6 +15,8 @@ public class Main {
 
     public static void main(String[] args)
     {
+        //안녕하세요 홍기표
+
         AcceptServer acceptServer = new AcceptServer();
         ServerLogPanel serverLogPanel = new ServerLogPanel();
         serverLogPanel.setStartEvent(new ActionListener() {
